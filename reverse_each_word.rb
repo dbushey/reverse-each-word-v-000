193a098fb1881475b72e3f39f1b562979f07f274
Daiane Bushey <daianebushey@gmail.com>
2017-12-30 22:51:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,10 @@
-# def reverse_each_word(sentence)
-#   arr = []
-#   rev_arr = []
-#   arr = sentence.split(" ")
-#   arr.each do |words|
-#     rev_arr << words.reverse!
-#   end
-#   rev_arr.*" "
-# end
-
 def reverse_each_word(sentence)
+  arr = []
+  rev_arr = []
   arr = sentence.split(" ")
   arr.collect do |words|
     words.reverse!
   end
 end
+
+
